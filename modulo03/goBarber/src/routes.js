0f146dd32bcb78e4dcc3842ee8c87a5936db3274
isaquevilson <isaque.vilson27@gmail.com>
2020-01-26 22:30:22 -0400

    Validation of schedules and listing user schedules

--- modulo03/goBarber/src/routes.js
+++ modulo03/goBarber/src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +18,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
