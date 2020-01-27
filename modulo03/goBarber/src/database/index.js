ddc51ad37ea84b9759de9798589069c0fb45c117
isaquevilson <isaque.vilson27@gmail.com>
2020-01-26 23:28:40 -0400

    configure mongoDB

--- modulo03/goBarber/src/database/index.js
+++ modulo03/goBarber/src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
@@ -9,6 +11,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -19,6 +22,17 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+            }
+        );
     }
 }
 
