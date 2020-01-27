d992fdeed4b1a1bcd64d53c3f57420644b63a4cf
isaquevilson <isaque.vilson27@gmail.com>
2020-01-26 22:35:52 -0400

    applying pagination

--- modulo03/goBarber/src/app/controllers/AppointmentController.js
+++ modulo03/goBarber/src/app/controllers/AppointmentController.js
@@ -11,9 +11,12 @@ class AppointmentController {
      * @param {*} res
      */
     async index(req, res) {
+        const { page = 1 } = req.query;
         const appointments = await Appointment.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
+            limit: 20,
+            offset: (page - 1) * 20,
             attributes: ['id', 'date'],
             include: [
                 {
