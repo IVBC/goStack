be749fab62f13a3a977da375c29f7dbc86c026fa
isaquevilson <isaque.vilson27@gmail.com>
2020-01-28 08:34:15 -0400

    mark notifications as read

--- modulo03/goBarber/src/routes.js
+++ modulo03/goBarber/src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,5 +26,8 @@ routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
 
+routes.get('/notifications', NotificationController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
